05353cc3bb6dec7a7384ebd860eebe74d9781870
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2020-07-15 09:19:12 +0200

    add login request

---
+++ Sources/GitHubModel/Request/LoginRequest.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amine Bensalah on 15/07/2020.
+//
+
+import Foundation
+
+public struct LoginRequest {
+    public let code: String
+    public let email: String?
+    public let name: String?
+    public let password: String?
+
+    public init(code: String, email: String?, name: String?, password: String?) {
+        self.code = code
+        self.email = email
+        self.name = name
+        self.password = password
+    }
+}
