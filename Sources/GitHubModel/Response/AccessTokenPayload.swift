56561b49ac46d4ae63a637bae085204412348675
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2020-08-11 15:05:01 +0200

    update format date for expired_at token

--- Sources/GitHubModel/Response/AccessTokenPayload.swift
+++ Sources/GitHubModel/Response/AccessTokenPayload.swift
@@ -5,9 +5,17 @@ public struct AccessTokenPayload: Codable {
     public let accessToken: String?
     public let expiredAt: String
 
-    public init(refreshToken: String, accessToken: String?, expiredAt: Date) {
+    public init(refreshToken: String, accessToken: String?, expiredAt: Date, dateFormat: DateFormatter = .default()) {
         self.refreshToken = refreshToken
         self.accessToken = accessToken
-        self.expiredAt = "\(expiredAt)"
+        self.expiredAt = dateFormat.string(from: expiredAt)
+    }
+}
+
+extension DateFormatter {
+    public static func `default`() -> DateFormatter {
+        let dateformatter = DateFormatter()
+        dateformatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS"
+        return dateformatter
     }
 }
--- Tests/GitHubModelTests/GitHubModelTests.swift
+++ Tests/GitHubModelTests/GitHubModelTests.swift
@@ -3,6 +3,19 @@ import XCTest
 
 final class GitHubModelTests: XCTestCase {
     func testExample() {
+        let now = Date()
+        let accessTokenPayload = AccessTokenPayload(
+            refreshToken: "test_refresh",
+            accessToken: "test_access",
+            expiredAt: now
+        )
+
+        guard let date = DateFormatter.default().date(from: accessTokenPayload.expiredAt) else {
+            XCTFail("can't create a date")
+            return
+        }
+
+        XCTAssertEqual(now.timeIntervalSince1970, date.timeIntervalSince1970, accuracy: 0.001)
     }
 
     static var allTests = [
