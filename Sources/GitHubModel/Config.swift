a97e6ec07792857a7811baf93fa43ddf51b51398
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2020-11-12 09:23:37 +0100

    update hook model

--- Sources/GitHubModel/Config.swift
+++ Sources/GitHubModel/Config.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct Config: Codable {
+public struct Config: Codable, Equatable {
     /// The media type used to serialize the payloads. Supported values include json and form. The default is form.
     public let contentType: String
     /// Determines whether the SSL certificate of the host for url will be verified when delivering payloads. Supported values include 0 (verification is performed) and 1 (verification is not performed). The default is 0. We strongly recommend not setting this to 1 as you are subject to man-in-the-middle and other attacks.
