59dc645ec8a1330b441ffaa8cdf39f5b10a9d6c8
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2020-12-26 07:23:24 +0100

    update access token model to use it without optional

--- Sources/GitHubModel/Response/AccessTokenPayload.swift
+++ Sources/GitHubModel/Response/AccessTokenPayload.swift
@@ -2,10 +2,10 @@ import Foundation
 
 public struct AccessTokenPayload: Codable {
     public let refreshToken: String
-    public let accessToken: String?
+    public let accessToken: String
     public let expiredAt: String
 
-    public init(refreshToken: String, accessToken: String?, expiredAt: Date, dateFormat: DateFormatter = .default()) {
+    public init(refreshToken: String, accessToken: String, expiredAt: Date, dateFormat: DateFormatter = .default()) {
         self.refreshToken = refreshToken
         self.accessToken = accessToken
         self.expiredAt = dateFormat.string(from: expiredAt)
